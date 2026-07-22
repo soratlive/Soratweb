c3cf670e993711a6db8f8466d894654ca93e05e2
Soratlive <soratlive@gmail.com>
2026-07-22 16:17:17 +0530

    Add files via upload

---
+++ android/app/src/main/java/com/sorat/game/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sorat.game
+
+import android.os.Bundle
+import com.getcapacitor.BridgeActivity
+
+class MainActivity : BridgeActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        registerPlugin(GoogleAuthPlugin::class.java)
+    }
+}
