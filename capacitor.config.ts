9c8253775325a3952f83f30a0902dff329808557
Soratlive <soratlive@gmail.com>
2026-07-23 12:54:22 +0530

    Configure GoogleAuth plugin in capacitor.config.ts
    Added GoogleAuth plugin configuration with scopes and refresh token settings.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,6 +6,13 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '',
+      forceCodeForRefreshToken: true,
+    }
   }
 };
 
